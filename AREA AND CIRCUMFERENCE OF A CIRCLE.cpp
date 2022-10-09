e5f69c1f731de375a8953c0c3a52eeb7d977ed5c
shubham yadav <115391493+shubham26yadav@users.noreply.github.com>
2022-10-09 12:48:55 +0530

    Add files via upload

---
+++ AREA AND CIRCUMFERENCE OF A CIRCLE.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float r,pi=3.14,c,a;
+	printf("Enter the radius =");
+	scanf("%f",&r);
+	
+	a=pi*r*r;
+	c=2*pi*r;
+	
+	printf("\nArea of a circle =%f",a);
+	printf("\nCircumference of a circle =%f",c);
+return 0;
+}
+	
+	
+
+
+
